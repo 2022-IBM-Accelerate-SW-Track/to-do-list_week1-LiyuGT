f4f6c673f99175da8303ede886738a5076de6d52
LiyuGT <liyutenaw@gmail.com>
2022-06-08 22:08:55 -0400

    Edit details

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,7 +19,6 @@ export default class About extends Component {
       <div className="centered">
         <div className="name_title">Liyu Tenaw</div>
         <div className="brief_description">
-          // Details about you goes here
           Hello. I am a rising junior at UNC Charlotte. I study Computer Science and I aspire to become Data Scientist. 
         </div>
       </div>
