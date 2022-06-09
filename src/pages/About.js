89d4c371b2903ccb8a48ad9ef7e4ce66ed6374b3
LiyuGT <liyutenaw@gmail.com>
2022-06-08 21:08:34 -0400

    Add Picture and Details

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import <profile_pic_name> from "/src/assets/headshot.jpeg" ;
 export default class About extends Component {
   render() {
     return (
       <div>
-      <p>My name is Liyu Tenaw! </p>
+      <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          // Image goes here
+          
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Liyu Tenaw</div>
+        <div className="brief_description">
+          // Details about you goes here
+          Hello. I am a rising junior at UNC Charlotte. I study Computer Science and I aspire to become Data Scientist. 
+        </div>
+      </div>
+    </div>
       </div>
     )
   }
